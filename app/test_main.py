c14eab4d1f84d6d0adbd6904a7871efdfcb18c9a
Oleksandr Shkabbura <shkabura007@gmail.com>
2026-07-23 00:15:24 +0300

    Cat&Dog

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+def test_if_result_0():
+    assert get_human_age(0, 0) == [0, 0]
+
+def test_almost_1():
+    assert get_human_age(14, 14) == [0, 0]
+
+def test_first_human_year():
+    assert get_human_age(15, 15) == [1, 1]
+
+def test_different_age_result():
+    assert get_human_age(28, 28) == [3, 2]
+
+def test_100_years():
+    assert get_human_age(100, 100) == [21, 17]
+
+def test_does_not_exchange_cat_dog():
+    assert get_human_age(24, 15) == [2, 1]
+
+def test_first_year_cat_dog():
+    assert get_human_age(23, 23) == [1, 1]
+
+def test_second_year_cat_dog():
+    assert get_human_age(24, 24) == [2, 2]
